2cd246fd813916c8902e894537bd2a2917a8da7b
clara <run_clara@yahoo.com>
2023-01-17 17:20:17 +0200

    fixes logo

--- first-test-app2/src/components/Header.jsx
+++ first-test-app2/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../assets/images/logo2.png';
+import Logo from '../assets/images/logo3.png';
 import { ReactComponent as ShoppingCart } from '../assets/icons/shopping-cart.svg';
 import { ReactComponent as FavoritesIcon } from '../assets/icons/favorites-icon2.svg';
 import './Header.css';
@@ -18,7 +18,7 @@ class Header extends React.Component{
             <header className="border-bottom mb-3">
                 <div className="container-fluid container-min-max-width d-flex justify-content-between align-items-center">
                     <Link to="/" className="my-3">
-                        <img src={Logo} alt="Sirluggia Shop" className="logo"/>
+                        <img src={Logo} alt="RoadFanaticShop" className="logo"/>
                     </Link>
                     <div>
                         { this.props.user
--- first-test-app2/src/pages/Login.jsx
+++ first-test-app2/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../assets/images/logo.png';
+import Logo from '../assets/images/logo3.png';
 import { ReactComponent as Google } from '../assets/icons/google.svg';
 import { ReactComponent as Facebook } from '../assets/icons/facebook-app-icon.svg';
 import './Login.css'
