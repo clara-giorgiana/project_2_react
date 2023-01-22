6643440fe747974453d5d0f60f44251f9789f9a1
clara <run_clara@yahoo.com>
2023-01-22 19:35:09 +0200

    small change in product item link (css)

--- first-test-app2/src/components/ProductItem.jsx
+++ first-test-app2/src/components/ProductItem.jsx
@@ -20,7 +20,7 @@ class ProductItem extends React.Component {
       <div className="product-item col-12 col-md-4 mb-3 d-flex flex-column align-items-center">
         <Link
           to={`/product/${id}`}
-          className="d-flex flex-column align-items-center"
+          className="btn-product-link"
         >
           <img src={image} alt="productPhoto" className="mb-2" />
           <p className="mb-1 text-center">{name}</p>
